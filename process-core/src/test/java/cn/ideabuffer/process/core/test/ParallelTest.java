f9a546527bef6eefc65192864f09d4f30d46ea66
sangjian.sj <sangjian.sj@alibaba-inc.com>
2020-05-06 11:21:53 +0800

    修改代码

--- process-core/src/test/java/cn/ideabuffer/process/core/test/ParallelTest.java
+++ process-core/src/test/java/cn/ideabuffer/process/core/test/ParallelTest.java
@@ -6,6 +6,7 @@ import cn.ideabuffer.process.core.ProcessInstance;
 import cn.ideabuffer.process.core.nodes.ParallelBranchNode;
 import cn.ideabuffer.process.core.nodes.ProcessNode;
 import cn.ideabuffer.process.core.nodes.builder.ParallelBranchNodeBuilder;
+import cn.ideabuffer.process.core.processors.DefaultParallelBranchProcessor;
 import cn.ideabuffer.process.core.strategy.ProceedStrategies;
 import cn.ideabuffer.process.core.test.nodes.parallel.TestParallelNodeProcessor1;
 import cn.ideabuffer.process.core.test.nodes.parallel.TestParallelNodeProcessor2;
@@ -20,9 +21,11 @@ public class ParallelTest {
     @Test
     public void testParallelNode() throws Exception {
         ProcessDefinition<String> definition = new DefaultProcessDefinition<>();
-        ParallelBranchNode node = ParallelBranchNodeBuilder.newBuilder().addBranch(
-            new ProcessNode<>(new TestParallelNodeProcessor1())).addBranch(
-            new ProcessNode<>(new TestParallelNodeProcessor2())).proceedWhen(ProceedStrategies.ALL_PROCEEDED).build();
+        ParallelBranchNode node = ParallelBranchNodeBuilder.newBuilder()
+            .addBranch(new ProcessNode<>(new TestParallelNodeProcessor1()))
+            .addBranch(new ProcessNode<>(new TestParallelNodeProcessor2()))
+            .by(new DefaultParallelBranchProcessor())
+            .proceedWhen(ProceedStrategies.ALL_PROCEEDED).build();
         definition.addProcessNodes(node);
         ProcessInstance<String> instance = definition.newInstance();
 
