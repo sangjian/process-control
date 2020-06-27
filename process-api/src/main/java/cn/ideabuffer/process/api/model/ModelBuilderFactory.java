abf3b7e2d28c7ad1409ab34277e33634e9317f93
sangjian.sj <sangjian.sj@alibaba-inc.com>
2020-06-27 11:03:49 +0800

    修改代码

--- process-api/src/main/java/cn/ideabuffer/process/api/model/ModelBuilderFactory.java
+++ process-api/src/main/java/cn/ideabuffer/process/api/model/ModelBuilderFactory.java
@@ -22,35 +22,35 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class ModelBuilderFactory {
 
-    private static ModelBuilderFactory INSTANCE = new ModelBuilderFactory();
+    private static final ModelBuilderFactory INSTANCE = new ModelBuilderFactory();
 
-    private Map<Class<?>, ModelBuilder<?>> builderMap;
+    private Map<Class, ModelBuilder> builderMap;
 
     private ModelBuilderFactory() {
         builderMap = new ConcurrentHashMap<>();
-        builderMap.putIfAbsent(Object.class, new ModelBuilder<>());
-        builderMap.putIfAbsent(Node.class, new NodeModelBuilder<>());
-        builderMap.putIfAbsent(ExecutableNode.class, new ExecutableNodeModelBuilder<>());
-        builderMap.putIfAbsent(BranchNode.class, new BranchNodeModelBuilder<>());
-        builderMap.putIfAbsent(Processor.class, new ProcessorModelBuilder<>());
-        builderMap.putIfAbsent(ComplexProcessor.class, new ComplexProcessorModelBuilder<>());
-        builderMap.putIfAbsent(BranchProcessor.class, new BranchProcessorModelBuilder<>());
-        builderMap.putIfAbsent(ProcessListener.class, new ProcessListenerModelBuilder<>());
-        builderMap.putIfAbsent(NodeListener.class, new NodeListenerModelBuilder<>());
-        builderMap.putIfAbsent(Rule.class, new RuleModelBuilder<>());
-        builderMap.putIfAbsent(ProcessDefinition.class, new ProcessDefinitionModelBuilder<>());
+        builderMap.putIfAbsent(Object.class, new ModelBuilder());
+        builderMap.putIfAbsent(Node.class, new NodeModelBuilder());
+        builderMap.putIfAbsent(ExecutableNode.class, new ExecutableNodeModelBuilder());
+        builderMap.putIfAbsent(BranchNode.class, new BranchNodeModelBuilder());
+        builderMap.putIfAbsent(Processor.class, new ProcessorModelBuilder());
+        builderMap.putIfAbsent(ComplexProcessor.class, new ComplexProcessorModelBuilder());
+        builderMap.putIfAbsent(BranchProcessor.class, new BranchProcessorModelBuilder());
+        builderMap.putIfAbsent(ProcessListener.class, new ProcessListenerModelBuilder());
+        builderMap.putIfAbsent(NodeListener.class, new NodeListenerModelBuilder());
+        builderMap.putIfAbsent(Rule.class, new RuleModelBuilder());
+        builderMap.putIfAbsent(ProcessDefinition.class, new ProcessDefinitionModelBuilder());
 
         // 条件节点
-        builderMap.putIfAbsent(IfConditionNode.class, new IfConditionNodeModelBuilder<>());
-        builderMap.putIfAbsent(IfProcessor.class, new IfProcessorModelBuilder<>());
+        builderMap.putIfAbsent(IfConditionNode.class, new IfConditionNodeModelBuilder());
+        builderMap.putIfAbsent(IfProcessor.class, new IfProcessorModelBuilder());
 
-        builderMap.putIfAbsent(WhileConditionNode.class, new WhileConditionNodeModelBuilder<>());
-        builderMap.putIfAbsent(DoWhileConditionNode.class, new DoWhileConditionNodeModelBuilder<>());
-        builderMap.putIfAbsent(WhileProcessor.class, new WhileProcessorModelBuilder<>());
-        builderMap.putIfAbsent(DoWhileProcessor.class, new DoWhileProcessorModelBuilder<>());
+        builderMap.putIfAbsent(WhileConditionNode.class, new WhileConditionNodeModelBuilder());
+        builderMap.putIfAbsent(DoWhileConditionNode.class, new DoWhileConditionNodeModelBuilder());
+        builderMap.putIfAbsent(WhileProcessor.class, new WhileProcessorModelBuilder());
+        builderMap.putIfAbsent(DoWhileProcessor.class, new DoWhileProcessorModelBuilder());
 
-        builderMap.putIfAbsent(TryCatchFinallyProcessor.class, new TryCatchFinallyProcessorModelBuilder<>());
-        builderMap.putIfAbsent(TryCatchFinallyNode.class, new TryCatchFinallyNodeModelBuilder<>());
+        builderMap.putIfAbsent(TryCatchFinallyProcessor.class, new TryCatchFinallyProcessorModelBuilder());
+        builderMap.putIfAbsent(TryCatchFinallyNode.class, new TryCatchFinallyNodeModelBuilder());
         builderMap.putIfAbsent(TryCatchFinallyNode.CatchMapper.class, new TryCatchFinallyProcessorModel.CatchMapperModelBuilder());
     }
 
@@ -87,8 +87,8 @@ public class ModelBuilderFactory {
         Class<?> parent = clazz.getSuperclass();
         Class<?>[] interfaces = clazz.getInterfaces();
         while ((parent != null && parent != Object.class) || interfaces.length > 0) {
-            for (int i = 0; i < interfaces.length; i++) {
-                builder = builderMap.get(interfaces[i]);
+            for (Class<?> anInterface : interfaces) {
+                builder = builderMap.get(anInterface);
                 if (builder != null) {
                     builderMap.putIfAbsent(clazz, builder);
                     return (M)builder;
