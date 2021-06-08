e135df0c1b4afae035747c3ecf0e05649cfb59bf
sangjian.sj <sangjian.sj@alibaba-inc.com>
2021-06-08 23:08:10 +0800

    修改继承接口

--- process-core/src/main/java/cn/ideabuffer/process/core/processors/DistributeProcessor.java
+++ process-core/src/main/java/cn/ideabuffer/process/core/processors/DistributeProcessor.java
@@ -1,12 +1,13 @@
 package cn.ideabuffer.process.core.processors;
 
+import cn.ideabuffer.process.core.Processor;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author sangjian.sj
  * @date 2020/05/09
  */
-public interface DistributeProcessor<T, R> extends ComplexProcessor<T> {
+public interface DistributeProcessor<T, R> extends Processor<T> {
 
     R merge(T t, @NotNull R r);
 }
